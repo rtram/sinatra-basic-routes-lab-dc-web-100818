5b02d076bff666fe4558eefd07b47fb2ce2641a6
Robin Tram <robin.tram@gmail.com>
2018-10-28 12:58:22 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get "/" do
+    "Hello, World!"
+  end
+
+  get "/name" do
+    "My name is Robin"
+  end
+
+  get "/hometown" do
+    "My hometown is Fairfax,Va"
+  end
+
+  get "/favorite-song" do
+    "My favorite song is Country Roads"
+  end
+
 end
